0a22d8607f84518aaac168adebb9d08d649b7263
alylmeier <alylmeier@gmail.com>
2022-12-19 14:08:45 -0500

    timer and questions working

--- assets/script.js
+++ assets/script.js
@@ -9,118 +9,122 @@ var btnA = document.getElementById('A')
 var btnB = document.getElementById('B')
 var btnC = document.getElementById('C')
 var btnD = document.getElementById('D')
-
+let quizContainer=document.getElementById("now")
 var quizFinished = true
+var secondsLeft = 30;
 
 var currentQuestion = 0;
 var score = 0;
 
-var quesOne = ["Which of the following is not a commonly part of the webpage code?"]
-/*var questions = [
+//var quesOne = ["Which of the following is not a commonly part of the webpage code?"]
+var questions = [
     {
         question: "Which of the following is not a commonly part of the webpage code?",
-            answers: [
-                {optionA:"HTML", answer:true},
-                {optionB:"Microsoft Edge", answer:false},
-                {optionC:"CSS", answer:true},
-                {optionD:"Javascript", answer:true},
-            ]
-        }
+        choices: ["HTML", "Microsoft Edge", "CSS", "Javascript"],
+        answer: "Microsoft Edge"
+    },
 
     {
         question: "This is filler 2?",
-            answers: [
-                {optionA:"HA", answer:true},
-                {optionB:"MA", answer:false},
-                {optionC:"A", answer:true},
-                {optionD:"JA", answer:true},
-            ]
-        }
+        choices: ["HA", "MA", "A", "JA"],
+        answer: "A"  
+    },
     {
         question: "This is filler 3?",
-        answers: [
-            {optionA:"HA", answer:true},
-            {optionB:"MA", answer:false},
-            {optionC:"A", answer:true},
-            {optionD:"JA", answer:true},
-            ]
-        }
-    {    
-        question: "This is filler 4?",
-            answers: [
-                {optionA:"HA", answer:true},
-                {optionB:"MA", answer:false},
-                {optionC:"A", answer:true},
-                {optionD:"JA", answer:true},
-            ]
-        }
-    {                
-        question: "This is filler 5?",
-            answers: [
-                {optionA:"HA", answer:true},
-                {optionB:"MA", answer:false},
-                {optionC:"A", answer:true},
-                {optionD:"JA", answer:true},
-            ]
-        }
-                    
+        choices: ["HA", "MA", "A", "JA"],
+        answer: "MA"
+      
+    },
+]
 
-    
-]*/
+function quiz() {
+    quizContainer.innerHTML=""
+   let question=questions[currentQuestion]
+//    Getting the values from an object using a . (dot) notations
+    // question = {
+    //     question: "Which of the following is not a commonly part of the webpage code?",
+    //     choices: ["HTML", "Microsoft Edge", "CSS", "Javascript"],
+    //     answer: "Microsoft Edge"
+    // }
+   let questionDiv=document.createElement("div")
+   questionDiv.setAttribute("id","qtext")
+   questionDiv.innerHTML=question.question
 
-function quiz(){
-    //currentQuestion = 0;
-    //current.textContent= "quesOne"; 
-    //document.getElementById("#now").textContent= quesOne
-    current.textContent="Which of the following is not a commonly part of the webpage code?";
+   let answerDiv=document.createElement("div")
+   answerDiv.setAttribute("id", "answers")
+    for(let i=0;i<question.choices.length;i++){
 
-    
-console.log(quesOne) 
+        let btnEl=document.createElement("button")
 
-    
+        btnEl.setAttribute("class", "btn")
+        btnEl.innerHTML=question.choices[i]
 
-  //  var myNewString = myString.replace("String", "World");
-
+        btnEl.addEventListener('click', function(event){
+            event.preventDefault()
+            console.log(event.target.innerHTML)
+            if(event.target.innerHTML===question.answer){
+                alert("Correct")
+                score=score+10;
+                console.log(score)
+                currentQuestion=currentQuestion+1;
+                quiz()
+            }
+            else{
+                alert("Incorrect Answer")
+                secondsLeft=secondsLeft-5
+                currentQuestion=currentQuestion+1;
+                quiz()
+            
+                
+            }
+        })
+        answerDiv.append(btnEl)
+    }
+    quizContainer.append(questionDiv,answerDiv)
 }
 
-function quizOver(){
+function quizOver() {
     window.alert("time is up")
 
 }
 
 // Selects element by class
-var timeEl = document.querySelector(".time");
+var timeEl = document.querySelector(".timeEl");
 
 // Selects element by id
 //var mainEl = document.getElementById("main");
 
-var secondsLeft = 30;
+
 
 function setTime() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left";
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left";
 
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
-    }
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+            // Calls function to create and append image
+            sendMessage();
+        }
 
-  }, 1000);
+    }, 1000);
 }
 function sendMessage() {
     timeEl.textContent = "All done!"
 }
+function init() {
+    setTime()
+    quiz()
+
+}
 
 
 
 
 
+start.addEventListener("click", init);
 
-start.addEventListener("click", quiz);
-start.addEventListener("click", setTime);
 
 
